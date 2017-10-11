cc83d85e24c151ed4d2f749fb1064161472ae2fd
Shady M <hakeem2008@hotmail.de>
2017-10-11 19:15:04 +0200

    button mit logik befüllt und datagrid bekommt die daten. geoobject erstellt

---
+++ Dojo_VO3/GeoObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo_VO3
+{
+    class GeoObject
+    {
+        public string Shape { get; set; }
+
+        public int PosX { get; set; }
+
+        public int PosY { get; set; }
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+
+        public GeoObject(string shape, int posX, int posY, int height, int width)
+        {
+            Shape = shape;
+            PosX = posX;
+            PosY = posY;
+            Height = height;
+            Width = width;
+        }
+    }
+}
--- Dojo_VO3/MainWindow.xaml.cs
+++ Dojo_VO3/MainWindow.xaml.cs
@@ -20,9 +20,25 @@ namespace Dojo_VO3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<GeoObject> Shapes = new List<GeoObject>();
+
         public MainWindow()
         {
             InitializeComponent();
+            cboForm.ItemsSource = new List<string>() {"Rechteck", "Kreis" };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Shapes.Add(new GeoObject(
+                cboForm.SelectedItem.ToString(), 
+                int.Parse(tboPosX.Text), 
+                int.Parse(tboPosY.Text), 
+                int.Parse(tboHeight.Text), 
+                int.Parse(tboWidth.Text)));
+            dtgrDataGrid.ItemsSource = null;
+            dtgrDataGrid.ItemsSource = Shapes;
+
         }
     }
 }
