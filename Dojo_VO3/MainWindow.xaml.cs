f240c550d3240dc8fb74599a1c6b2b74c50f1bde
Shady M <hakeem2008@hotmail.de>
2017-10-11 20:18:56 +0200

    MVVM_Beginning_Template_With_FormPosXY etc

--- Dojo_VO3/MainWindow.xaml.cs
+++ Dojo_VO3/MainWindow.xaml.cs
@@ -20,64 +20,11 @@ namespace Dojo_VO3
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<GeoObject> Shapes = new List<GeoObject>();
 
         public MainWindow()
         {
             InitializeComponent();
-            cboForm.ItemsSource = new List<string>() {"Rechteck", "Kreis" };
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Shapes.Add(new GeoObject(
-                cboForm.SelectedItem.ToString(), 
-                int.Parse(tboPosX.Text), 
-                int.Parse(tboPosY.Text), 
-                int.Parse(tboHeight.Text), 
-                int.Parse(tboWidth.Text)));
-            dtgrDataGrid.ItemsSource = null;
-            dtgrDataGrid.ItemsSource = Shapes;
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            var temp = dtgrDataGrid.SelectedItem as GeoObject;
-            Shape geoObject = null;
-
-            switch (temp.Shape)
-            {
-                case "Rechteck":
-                    geoObject = new Rectangle()
-                    { 
-                        Height = temp.Height,
-                        Width = temp.Width,
-                        Stroke = new SolidColorBrush(Colors.Red),
-                        StrokeThickness = 1
-                    };
-
-                    break;
-                case "Kreis":
-                    geoObject = new Ellipse()
-                    {
-                        Height = temp.Height,
-                        Width = temp.Width,
-                        Stroke = new SolidColorBrush(Colors.Red),
-                        StrokeThickness = 1
-                    };
-                    break;
-                default:
-                    break;
-            }
-            canDrawingArea.Children.Add(geoObject);
-            geoObject.SetValue(Canvas.TopProperty, (double)temp.PosY);
-            geoObject.SetValue(Canvas.LeftProperty, (double)temp.PosX);
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
