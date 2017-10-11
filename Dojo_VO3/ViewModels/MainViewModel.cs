7b8cf3b5f3b23971c4eb463d7d74a952699e9444
Shady M <hakeem2008@hotmail.de>
2017-10-11 20:36:53 +0200

    formbox gebindet zum MVM hin

---
+++ Dojo_VO3/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo_VO3.ViewModels
+{
+    class MainViewModel
+    {
+        public List<String> Shapes { get; set; } //property!!
+
+        public MainViewModel()
+        {
+            Shapes = new List<string>() { "Rechteck", "Kreis" };
+        }
+    }
+}
